24321d831002fb8335b0a44dcdf3c6e39d1ae1ce
Edmar233 <edmargois@gmail.com>
2025-08-21 11:58:18 -0300

    exercicio

--- Java Script/exercicio.js
+++ Java Script/exercicio.js
@@ -1,10 +1,10 @@
-const numero1 = prompt ("Digite um numero: ")
-const numero2 = prompt ("Digite outro numero: ")
-const soma =  Number(numero1) + Number(numero2)
+const numero1 = parseFloat(prompt ("Digite um numero: "))
+const numero2 = parseFloat(prompt ("Digite outro numero: "))
+const soma =  numero1 + numero2
 
 console.log("A soma dos 2 números é: " + soma)
 
 document.writeln("A soma dos 2 números é: " + soma)
 
 alert("A soma dos 2 números é: " + soma)
-
+alert(`A soma de ${numero1} com ${numero2} é igual a ${soma}.`)
