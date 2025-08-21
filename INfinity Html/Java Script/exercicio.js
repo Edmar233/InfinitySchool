a14028dfa4daa134eaa3c34329b3e6d227264c3c
Edmar233 <edmargois@gmail.com>
2025-08-21 11:37:24 -0300

    Aula01JavaScript

---
+++ INfinity Html/Java Script/exercicio.js
@@ -0,0 +1,10 @@
+const numero1 = prompt ("Digite um numero: ")
+const numero2 = prompt ("Digite outro numero: ")
+const soma =  Number(numero1) + Number(numero2)
+
+console.log("A soma dos 2 números é: " + soma)
+
+document.writeln("A soma dos 2 números é: " + soma)
+
+alert("A soma dos 2 números é: " + soma)
+
