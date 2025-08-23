0dca7cac7e095103214a9ec61dea4d685931fa2d
Edmar233 <edmargois@gmail.com>
2025-08-22 22:44:08 -0300

    Add files via upload

---
+++ INfinity Html/Infinity Full Stack/Java Script/exercicio.js
@@ -0,0 +1,10 @@
+const numero1 = parseFloat(prompt ("Digite um numero: "))
+const numero2 = parseFloat(prompt ("Digite outro numero: "))
+const soma =  numero1 + numero2
+
+console.log("A soma dos 2 números é: " + soma)
+
+document.writeln("A soma dos 2 números é: " + soma)
+
+alert("A soma dos 2 números é: " + soma)
+alert(`A soma de ${numero1} com ${numero2} é igual a ${soma}.`)
---
+++ INfinity Html/Infinity Full Stack/Java Script/variaveis.js
@@ -0,0 +1,10 @@
+//var cpf 
+//cpf = "12345678988"
+//alert(cpf)
+
+//cpf = "12345678877"
+//alert(cpf)
+
+//const cpf = "12345678877"
+//alert(cpf)
+
