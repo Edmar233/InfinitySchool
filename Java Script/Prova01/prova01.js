6a9f0b14d145654df474cb349a9ee03dbe665676
Edmar233 <edmargois@gmail.com>
2025-09-04 18:17:37 -0300

    prova java

--- Java Script/Prova01/prova01.js
+++ Java Script/Prova01/prova01.js
@@ -9,34 +9,31 @@ console.log(`A divisão entre os numeros ${numero1} e ${numero2} é : ${numero1 
 console.log(`O resto da divisão entre os numeros ${numero1} e ${numero2} é : ${numero1 % numero2}`)
 console.log("")
 
-console.log("Resultado com Atribuição \n")
+console.log("Resultado com Atribuição ")
 console.log("")
+let result = numero1;
 // Atribuição Adição
-let soma = numero1;
-soma += numero2;
-console.log(`A soma entre os numeros ${numero1} e ${numero2} é : ${soma}`)
+result += numero2;
+console.log(`A soma entre os numeros ${numero1} e ${numero2} é : ${result}`)
 
 // Atribuição Subtração
-let subtracao = numero1;
-subtracao -= numero2;
-console.log(`A subtração entre os numeros ${numero1} e ${numero2} é : ${subtracao}`)
+result = numero1;//reseta ao valor original 
+result -= numero2;
+console.log(`A subtração entre os numeros ${numero1} e ${numero2} é : ${result}`)
 
 // Atribuição Multiplicação
-let multiplicacao = numero1;
-multiplicacao *= numero2;
-console.log(`A multiplicação entre os numeros ${numero1} e ${numero2} é : ${multiplicacao}`)
+result = numero1;
+result *= numero2;
+console.log(`A multiplicação entre os numeros ${numero1} e ${numero2} é : ${result}`)
 
 // Atribuição Divisão
-let divisao = numero1;
-divisao /= numero2;
-console.log(`A divisão entre os numeros ${numero1} e ${numero2} é : ${divisao}`)
+result = numero1;
+result /= numero2;
+console.log(`A divisão entre os numeros ${numero1} e ${numero2} é : ${result}`)
 
 // Atribuição Resto
-let resto = numero1;
-resto %= numero2;
-console.log(`O resto da divisão entre os numeros ${numero1} e ${numero2} é : ${resto}`)
+result = numero1;
+result %= numero2;
+console.log(`O resto da divisão entre os numeros ${numero1} e ${numero2} é : ${result}`)
 
 
-// Realize as operações aritméticas básicas (adição, subtração, multiplicação, divisão e resto) e exiba os resultados.
-// Utilize operadores de atribuição (como +=, -=, *=, etc.) para atualizar/reatribuir o valor de uma variável com os resultados das operações.
-// Mostre os resultados no console utilizando console.log().
