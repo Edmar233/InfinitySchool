a33ee0d13a75ecdda5befb7821c04b018a1be929
Edmar233 <edmargois@gmail.com>
2025-09-25 08:03:56 -0300

    att

--- Java Script/aula03/exercicioCasa.js
+++ Java Script/aula03/exercicioCasa.js
@@ -1,17 +1,18 @@
 // CRIE UM PROGRAMA QUE PEÇA UMA FRASE AO USUÁRIO E VERIFIQUE SE ELA É UM PALINDROMO
 
-let frase = prompt("Digite uma palavra: ").toLocaleLowerCase()
+let palavra = prompt("Digite uma palavra: ").toLocaleLowerCase()
 let reverse = "";
 
-for (let letra of frase.toLowerCase().replaceAll(" ","")) {
+for (let letra of palavra) {
   reverse = letra + reverse;
 }
 alert(`A palvara invertida é : ${reverse}`)
 
-if (reverse === frase.toLowerCase().replaceAll(" ", "")) {
+if (palavra === reverse) {
   alert("É um palíndromo!");
 } else {
   alert("Não é um palíndromo!");
 }
+
 
 
---
+++ Java Script/aula06function/exercicio04.js
@@ -0,0 +1,18 @@
+function saudacao(){
+    let nomes = [];
+    let nome = "";
+
+    while (nome !== "0" ) {
+        nome = (prompt(`Digite o nome: \n ou \n digite "0" para encerrar o programa)`));
+        
+        if (nome === "0") {
+            console.log(`Programa encerrado. Até mais!`);
+            break
+        
+        }
+        nomes.push(nome);
+        console.log(`Seja(m) bem vindo(s) !!! ${nome.join(", ")}`);
+        
+    }
+}
+saudacao();
